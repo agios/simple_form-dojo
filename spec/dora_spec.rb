eb403dc29f6df610bbcccd9192558e257347a161
Patrick Ward <patrick@patrickward.com>
2011-02-28 18:49:48 -0500

    Initial commit

---
+++ spec/dora_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Dora do
+  it "should be valid" do
+    Dora.should be_a(Module)
+  end
+end
---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '18e00c764b86014394b1306d2a7deb70abd7083b71d9803ccd5fb39b1df02eb7827e0df3422c7653718a91243176ee129d2c52bed24ee6668ec47b95963e0d2d'
