c86df245e86a5c54325e95b3e105c9676eca0ea6
Patrick Ward <patrick@patrickward.com>
2011-03-19 15:05:47 -0400

    Removing spec

--- spec/helpers/dora_form_builder_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe 'Dora::FormBuilder Test', :type => :helper do
-  def with_form_for(object, *args, &block)
-    with_concat_form_for(object) do |f|
-    end
-  end
-end
