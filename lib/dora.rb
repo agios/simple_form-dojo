8da2b91383aef65d418b3acbc68d774f47e5a660
Patrick Ward <patrick@patrickward.com>
2011-03-20 14:47:41 -0400

    ...

--- lib/dora.rb
+++ lib/dora.rb
@@ -3,7 +3,6 @@ require_relative 'dora/version'
 require_relative 'dora/helpers/tabs'
 
 module Dora
-  autoload :FormBuilder,    'dora/form_builder'
 end
 
 ActionView::Base.send(:include, Dora::Helpers::TabsHelper)
